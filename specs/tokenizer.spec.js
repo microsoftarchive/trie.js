493ec08fbad296a81d4dfa9cd5f75e5e0dc5395c
Aditya <aditya@netroy.in>
2013-03-19 21:17:13 +0100

    specs stub

---
+++ specs/tokenizer.spec.js
@@ -0,0 +1,15 @@
+describe('Tokinizers', function() {
+
+  'use strict';
+
+  describe('Whitespace Tokinizer', function() {
+
+    beforeEach(function() {
+
+    });
+
+    it('should return words from a sentence', function() {
+      (5).should.be.equal(5);
+    });
+  });
+});
